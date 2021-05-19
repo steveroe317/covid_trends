b2f836b2695e297475aabb34fc61cf13f57ab4b1
Steve Roe <steveroe@gmail.com>
2021-05-18 22:45:01 -0700

    Added firebase loading and initialization pages.
    These pages are informative ony, they  don't allow for any user
    action.

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'models/covid_timeseries_model.dart';
 import 'widgets/covid_entities_page.dart';
-//import 'widgets/simple_chart_page.dart';
-
-//import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,14 +21,14 @@ class MyApp extends StatelessWidget {
       future: _initialization,
       builder: (context, snapshot) {
         if (snapshot.hasError) {
-          return CircularProgressIndicator(backgroundColor: Colors.red);
+          return InitializationErrorPage();
         }
         if (snapshot.connectionState == ConnectionState.done) {
           Provider.of<CovidTimeseriesModel>(context, listen: false)
               .initialize();
           return buildMaterialApp();
         }
-        return CircularProgressIndicator(backgroundColor: Colors.green);
+        return LoadingPage();
       },
     );
   }
@@ -50,17 +47,34 @@ class MyApp extends StatelessWidget {
 class LoadingPage extends StatelessWidget {
   @override
   build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Loading')),
-    );
+    return Container(
+        color: Colors.grey,
+        child: Center(
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Loading Covid Trends',
+                  textDirection: TextDirection.ltr, textScaleFactor: 2.0)),
+          Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(
+                  backgroundColor: Colors.blue.shade200)),
+        ])));
   }
 }
 
 class InitializationErrorPage extends StatelessWidget {
   @override
   build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Error initializing Firebase')),
+    return Container(
+      color: Colors.grey,
+      child: Center(
+          child: Text('Error loading Covid Trends',
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.center,
+              textScaleFactor: 2.0,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold))),
     );
   }
 }
