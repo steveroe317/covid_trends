078d77931ac6f3fc139f149026ac5e703e2f7a78
Steve Roe <steveroe@gmail.com>
2021-06-06 15:20:48 -0700

    Add firebase anonymous authentication.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'models/covid_timeseries_model.dart';
@@ -27,6 +28,8 @@ class MyApp extends StatelessWidget {
           return InitializationErrorPage();
         }
         if (snapshot.connectionState == ConnectionState.done) {
+          FirebaseAuth auth = FirebaseAuth.instance;
+          auth.signInAnonymously();
           Provider.of<CovidTimeseriesModel>(context, listen: false)
               .initialize();
           return buildMaterialApp();
