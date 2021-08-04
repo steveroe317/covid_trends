b860f81603407ad6508d448634950ac10dbd9919
Steve Roe <steveroe@gmail.com>
2021-08-04 16:04:34 -0700

    Split entity list out of covid entities page.

--- lib/widgets/covid_entities_page.dart
+++ lib/widgets/covid_entities_page.dart
@@ -1,7 +1,6 @@
 import 'package:covid_trends/widgets/ui_parameters.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import '../models/covid_entities_page_model.dart';
@@ -9,16 +8,14 @@ import '../models/covid_timeseries_model.dart';
 import 'compare_region_popup_menu.dart';
 import 'covid_chart_group.dart';
 import 'covid_chart_group_page.dart';
+import 'covid_entities_list.dart';
 import 'date_range_popup_menu.dart';
 //import 'debug_popup_menu.dart';
 import 'per_100k_popup_menu.dart';
 import 'share_button.dart';
 import 'sort_popup_menu.dart';
 import 'star_popup_menu.dart';
-import 'ui_constants.dart';
 import 'ui_parameters.dart';
-
-enum _CovidEntityListItemDepth { root, stem, leaf }
 
 class CovidEntitiesPage extends StatefulWidget {
   CovidEntitiesPage({Key key, this.title}) : super(key: key);
@@ -136,208 +133,5 @@ class _CovidEntitiesNarrowListBody extends StatelessWidget {
     }
 
     return CovidEntityList(onRegionPressed);
-  }
-}
-
-class CovidEntityList extends StatelessWidget {
-  final void Function(CovidTimeseriesModel, List<String>) _onRegionPressed;
-
-  CovidEntityList(this._onRegionPressed);
-
-  @override
-  build(BuildContext context) {
-    var timeseriesModel = Provider.of<CovidTimeseriesModel>(context);
-    var pageModel = Provider.of<CovidEntitiesPageModel>(context);
-    var uiParameters = context.read<UiParameters>();
-    final childNames = timeseriesModel.entityChildNames(pageModel.path(),
-        sortBy: pageModel.sortMetric,
-        sortUp: false,
-        per100k: pageModel.per100k);
-    final currentPath = pageModel.path();
-    final numberFormatter = NumberFormat('#,###');
-
-    List<Widget> entityList = [];
-
-    for (var index = 0; index < currentPath.length; ++index) {
-      final path = currentPath.sublist(0, index + 1);
-      var depth = (index == 0)
-          ? _CovidEntityListItemDepth.root
-          : _CovidEntityListItemDepth.stem;
-      entityList.add(EntityListItem(path, depth, _onRegionPressed, pageModel,
-          timeseriesModel, numberFormatter));
-    }
-
-    if (currentPath.length > 0) {
-      entityList.add(Divider());
-    }
-
-    entityList.addAll(List<Widget>.from(childNames.map((name) => EntityListItem(
-        [...currentPath, name],
-        _CovidEntityListItemDepth.leaf,
-        _onRegionPressed,
-        pageModel,
-        timeseriesModel,
-        numberFormatter))));
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: uiParameters.entityRowWidth,
-          child: EntityListHeader(pageModel),
-        ),
-        Container(child: Divider()),
-        Expanded(child: ListView(children: entityList)),
-      ],
-    );
-  }
-}
-
-class EntityListHeader extends StatelessWidget {
-  final CovidEntitiesPageModel _pageModel;
-
-  EntityListHeader(this._pageModel);
-
-  @override
-  build(BuildContext context) {
-    var uiParameters = context.read<UiParameters>();
-    return SizedBox(
-        width: uiParameters.entityRowWidth,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Opacity(
-              opacity: 0.0,
-              child: IconButton(
-                icon: Icon(Icons.expand_more),
-                onPressed: () {},
-              ),
-            ),
-            SizedBox(
-              width: uiParameters.entityButtonWidth,
-              child: TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                    alignment: AlignmentDirectional(0, 0)),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('Region',
-                        style: Theme.of(context).textTheme.headline6)),
-              ),
-            ),
-            SizedBox(
-              width: uiParameters.entityMetricWidth,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(_sortMetricName()),
-              ),
-            ),
-          ],
-        ));
-  }
-
-  String _sortMetricName() {
-    var name = (_pageModel.sortMetric != UiConstants.noSortMetricName)
-        ? _pageModel.sortMetric
-        : UiConstants.defaultDisplayMetric;
-    if (_pageModel.per100k) {
-      name = '$name\nper 100,000';
-    }
-    return name;
-  }
-}
-
-class EntityListItem extends StatelessWidget {
-  final List<String> _path;
-  final _CovidEntityListItemDepth _depth;
-  final void Function(CovidTimeseriesModel, List<String>) _onRegionPressed;
-  final CovidTimeseriesModel _timeseriesModel;
-  final CovidEntitiesPageModel _pageModel;
-  final numberFormatter;
-
-  EntityListItem(this._path, this._depth, this._onRegionPressed,
-      this._pageModel, this._timeseriesModel, this.numberFormatter);
-
-  void onRegionPressed() {
-    return _onRegionPressed(_timeseriesModel, _path);
-  }
-
-  @override
-  build(BuildContext context) {
-    var pageModel = Provider.of<CovidEntitiesPageModel>(context);
-    var uiParameters = context.read<UiParameters>();
-    return Container(
-        width: uiParameters.entityRowWidth,
-        color: listEquals(_path, pageModel.chartPath()) ? Colors.black12 : null,
-        padding: EdgeInsets.only(left: 6, right: 6),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Opacity(
-              opacity: (_depth != _CovidEntityListItemDepth.root &&
-                      _timeseriesModel.entityHasChildren(_path))
-                  ? 1.0
-                  : 0.0,
-              child: IconButton(
-                  icon: Icon(_depth == _CovidEntityListItemDepth.leaf
-                      ? Icons.expand_more
-                      : Icons.expand_less),
-                  onPressed: (_depth == _CovidEntityListItemDepth.stem)
-                      ? _openParentPath
-                      : (_depth == _CovidEntityListItemDepth.leaf &&
-                              _timeseriesModel.entityHasChildren(_path))
-                          ? _openPath
-                          : null),
-            ),
-            SizedBox(
-              width: uiParameters.entityButtonWidth,
-              child: TextButton(
-                onPressed: onRegionPressed,
-                onLongPress: onRegionLongPress,
-                style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
-                    alignment: AlignmentDirectional(0, 0)),
-                child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(_path.last,
-                        style: Theme.of(context).textTheme.headline6)),
-              ),
-            ),
-            SizedBox(
-              width: uiParameters.entityMetricWidth,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Text(_metricValueString()),
-              ),
-            ),
-          ],
-        ));
-  }
-
-  void onRegionLongPress() {
-    _pageModel.clearPathList();
-    onRegionPressed();
-  }
-
-  void _openParentPath() {
-    _pageModel.setPath(_path.sublist(0, _path.length - 1));
-  }
-
-  void _openPath() {
-    _timeseriesModel.loadEntity(_path);
-    _pageModel.setPath(_path);
-  }
-
-  String _metricValueString() {
-    String displayMetric = _pageModel.sortMetric;
-    if (displayMetric == UiConstants.noSortMetricName) {
-      displayMetric = UiConstants.defaultDisplayMetric;
-    }
-    var metricValue = _timeseriesModel.entitySortMetric(
-        _path, displayMetric, _pageModel.per100k);
-    return numberFormatter.format(metricValue);
   }
 }
