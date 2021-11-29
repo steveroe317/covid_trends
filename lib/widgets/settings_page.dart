95e7950e10b2d5abaccfbfeaa904f95d7bf023ef
Steve Roe <steveroe@gmail.com>
2021-11-29 12:12:05 -0800

    Add last loaded data date to settings.

--- lib/widgets/settings_page.dart
+++ lib/widgets/settings_page.dart
@@ -13,18 +13,27 @@
 // limitations under the License.import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 import '../models/app_display_state_model.dart';
+import '../models/model_constants.dart';
 import '../models/starred_chart_examples.dart';
+import '../models/covid_timeseries_model.dart';
 import '../theme/size_scale.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   build(BuildContext context) {
-    var pageModel = Provider.of<AppDisplayStateModel>(context);
+    var pageModel = Provider.of<AppDisplayStateModel>(context, listen: false);
+    var timeseriesModel =
+        Provider.of<CovidTimeseriesModel>(context, listen: false);
 
     var settingsItems = <Widget>[];
+    settingsItems.add(ListTile(
+      leading: Icon(Icons.info),
+      title: Text('${_lastRootDataMessage(timeseriesModel)}'),
+    ));
     settingsItems.add(ListTile(
       leading: Icon(Icons.refresh),
       title: Text('Reload Saved Example Charts',
@@ -40,5 +49,15 @@ class SettingsPage extends StatelessWidget {
           child: ListView(
               padding: EdgeInsets.all(SizeScale.px4), children: settingsItems),
         ));
+  }
+
+  String _lastRootDataMessage(CovidTimeseriesModel timeseriesModel) {
+    var timestamps =
+        timeseriesModel.entityTimestamps([ModelConstants.rootEntityName]);
+    if (timestamps.isEmpty) {
+      return 'No Covid data loaded';
+    }
+    var date = DateTime.fromMillisecondsSinceEpoch(1000 * timestamps.last);
+    return 'Covid data though ${DateFormat('yyyy-MM-dd').format(date)}';
   }
 }
